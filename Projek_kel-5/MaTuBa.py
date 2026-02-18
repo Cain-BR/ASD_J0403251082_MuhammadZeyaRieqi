475ff9d307680c8550c948f4e00c3d4fab2ffcba
Cain-BR <fortunezeya@gmail.com>
2026-02-18 16:38:47 +0700

    Projek2

---
+++ Projek_kel-5/MaTuBa.py
@@ -0,0 +1,16 @@
+#Projek kelompok 5
+#Manajemen Tumpukan Gudang Barang Berat Menggunakan Struktur Data Stack
+#Muhammad Danish Rahmanu Ghaisan (J0403251124)
+#Muhammad Zeya Rieqi (J0403251082)
+#Nelson Arsel Warae (J0403251061)
+#Algoritma dan Struktur Data
+#Teknologi Rekayasa Perangkat Lunak
+#Tahun Akademik 2025/2026
+
+#1. Program stack
+#2. Program menu
+
+class barang:
+    def __init__(self, nama, berat):
+        self.nama = nama
+        self.berat = berat
